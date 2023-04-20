b0f1cd41df99d6625b06a90b07636e281ebccef7
Di Di <>
2023-04-20 20:51:08 +0700

    Fix some bugs Order API

--- src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
+++ src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
@@ -1,7 +1,11 @@
+using Contracts.Common.Interfaces;
+using Infrastructure.Common;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Common.Interfaces;
 using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
 
 namespace Ordering.Infrastructure;
 
@@ -13,6 +17,8 @@ public static class ConfigureServices
             builder => builder.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)));
 
         services.AddScoped<OrderContextSeed>(); // Seed data when start Ordering.API
+        services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
         
         return services;
     }
