af4b8d6b1a0d25feacb4145dabe9026e3fdefa5b
Di Di <>
2023-06-25 21:08:40 +0700

    Completed section Hangfire and Background Job

--- src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
+++ src/Services/Ordering/Ordering.Infrastructure/ConfigureServices.cs
@@ -1,6 +1,7 @@
 using Contracts.Common.Interfaces;
 using Contracts.Services;
 using Infrastructure.Common;
+using Infrastructure.Extensions;
 using Infrastructure.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Ordering.Application.Common.Interfaces;
 using Ordering.Infrastructure.Persistence;
 using Ordering.Infrastructure.Repositories;
+using Shared.Configurations;
 
 namespace Ordering.Infrastructure;
 
@@ -15,8 +17,16 @@ public static class ConfigureServices
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<OrderContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"),
-            builder => builder.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)));
+        var databaseSettings = services.GetOptions<DatabaseSettings>(nameof(DatabaseSettings));
+        if (databaseSettings == null || string.IsNullOrEmpty(databaseSettings.ConnectionString))
+            throw new ArgumentNullException($"Connection string is not configured");
+        
+        services.AddDbContext<OrderContext>(options =>
+        {
+            options.UseSqlServer(databaseSettings.ConnectionString,
+                builder => 
+                    builder.MigrationsAssembly(typeof(OrderContext).Assembly.FullName));
+        });
 
         services.AddScoped<OrderContextSeed>(); // Seed data when start Ordering.API
         services.AddScoped<IOrderRepository, OrderRepository>();
